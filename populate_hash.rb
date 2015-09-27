3bcd6bf50929aaaf277e673518f776e331679599
Abe Petrillo <abe.petrillo@gmail.com>
2015-09-27 18:42:51 +0100

    switch to RubyProf

--- populate_hash.rb
+++ populate_hash.rb
@@ -1,25 +1,32 @@
 require 'benchmark'
 require 'pry'
+require 'ruby-prof'
+RubyProf.measure_mode = RubyProf::MEMORY
+#RubyProf.measure_mode = RubyProf::CPU_TIME
 
 Foo = Struct.new(:rating)
 arr = []
-arr.push (0...999).map {|i| Foo.new(rating:  {client: 1})}
-arr.push (0...999).map {|i| Foo.new(rating: {bad:1})}
-arr.push (0...999).map {|i| Foo.new(rating: {ugly: 1})}
+arr.push (0...99999).map {|i| Foo.new(rating:  {client: 1})}
+arr.push (0...99999).map {|i| Foo.new(rating: {bad:1})}
+arr.push (0...99999).map {|i| Foo.new(rating: {ugly: 1})}
 a = arr.flatten
 
-puts Benchmark.bm  { |x|
-  x.report('reduce') {
-    a.reduce({}) do |hsh, o|
-      hsh[:rating] = o.rating
-    end
-  }
+result2 = RubyProf.profile do
+  a.each_with_object({}) do |o, hsh|
+    hsh[:rating] = o.rating
+  end
+end
 
-  x.report('each_with_object') {
-    a.each_with_object({}) do |o, hsh|
-      hsh[:rating] = o.rating
-    end
-  }
+result = RubyProf.profile do
+  a.reduce({}) do |hsh, o|
+    hsh[:rating] = o.rating
+  end
+end
 
 
-}
+
+printer = RubyProf::FlatPrinter.new(result)
+printer.print(STDOUT, {})
+
+printer = RubyProf::FlatPrinter.new(result2)
+printer.print(STDOUT, {})
